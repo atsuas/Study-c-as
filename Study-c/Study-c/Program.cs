8c407e8c9bf8f87343a3884abb70268b21d61b17
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-23 00:35:45 +0900

    都市名を入力するとindexを返してくれる　練習

--- Study-c/Study-c/Program.cs
+++ Study-c/Study-c/Program.cs
@@ -8,8 +8,25 @@ namespace Study_c
     {
         static void Main(string[] args)
         {
-            
+            var names = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong", };
+
+            One(names);
+            Console.WriteLine("-----");
+
         }
 
+        private static void One(List<string> names)
+        {
+            Console.WriteLine("都市名を入力。空文字で終了");
+            do
+            {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;
+                var index = names.FindIndex(s => s == line);
+                Console.WriteLine(index);
+            } while (true);
+
+        }
     }
 }
