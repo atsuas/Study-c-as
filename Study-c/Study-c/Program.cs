223d172b4a78bc89dd7c7880f3a0867170007b16
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-22 23:35:51 +0900

    foreachを使って2.0で割った数値を表示　練習

--- Study-c/Study-c/Program.cs
+++ Study-c/Study-c/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Study_c
 {
@@ -6,7 +8,32 @@ namespace Study_c
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            One(numbers);
+            Console.WriteLine("-----");
+
+            Two(numbers);
+            Console.WriteLine("-----");
+        }
+
+        private static void One(List<int> numbers)
+        {
+            var exists = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            if (exists)
+                Console.WriteLine("存在します");
+            else
+                Console.WriteLine("存在しません");
+        }
+
+        private static void Two(List<int> numbers)
+        {
+            double s = 2.0;
+            foreach (double num in numbers)
+            {
+                var n = num / s;
+                Console.WriteLine(n);
+            }
         }
     }
 }
