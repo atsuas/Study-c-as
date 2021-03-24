91e3ab6379eec7e34deec4b66ebd364730fdd2e6
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-24 14:33:39 +0900

    8か9で割り切れたら表示する　練習

--- Study-c/Study-c/Program.cs
+++ Study-c/Study-c/Program.cs
@@ -10,8 +10,25 @@ namespace Study_c
     {
         static void Main(string[] args)
         {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48, 18 };
 
+            One(numbers);
+            Console.WriteLine("-----");
+        }
 
+        private static void One(List<int> numbers)
+        {
+            foreach (var num in numbers.Where(n => n % 8 == 0 || n % 9 == 0))
+            {
+                if (num == num)
+                {
+                    Console.WriteLine($"{num} 8or9で割り切れます");
+                }
+                else
+                {
+                    Console.WriteLine("存在していません");
+                }
+            }
         }
 
     }
