000bddc40f47b782997edbdd45b81b8a0bc7a3bb
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-25 23:38:41 +0900

    YearMonthクラス　練習

--- Study-c/Study-c/Program.cs
+++ Study-c/Study-c/Program.cs
@@ -14,5 +14,42 @@ namespace Study_c
         }
 
     }
+
+    class YearMonth
+    {
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        public YearMonth(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+
+        public bool Is21Century
+        {
+            get
+            {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        public YearMonth AddOneMonth()
+        {
+            if (Month == 12)
+            {
+                return new YearMonth(this.Year + 1, 1);
+            }
+            else
+            {
+                return new YearMonth(this.Year, this.Month + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Year}年 {Month}月";
+        }
+    }
 }
  
