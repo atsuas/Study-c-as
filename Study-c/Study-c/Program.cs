1ec44d0df52965f88cd3e786ae3e9aa9edf7995c
DESKTOP-0QC9CU8\aoyuu <aoyuu.920.96@gmail.com>
2021-03-26 02:29:05 +0900

    不要なコードの削除

--- Study-c/Study-c/Program.cs
+++ Study-c/Study-c/Program.cs
@@ -10,107 +10,11 @@ namespace Study_c
     {
         static void Main(string[] args)
         {
-            var yearMonth = new YearMonth[]
-            {
-                new YearMonth(1990, 8),
-                new YearMonth(1999, 7),
-                new YearMonth(2000, 3),
-                new YearMonth(2020, 4),
-                new YearMonth(2035, 2),
-            };
-
-            One(yearMonth);
-            Console.WriteLine("-----");
-
-            Two(yearMonth);
-            Console.WriteLine("-----");
-
-            Three(yearMonth);
-            Console.WriteLine("-----");
-
-
-            static void One(YearMonth[] ymCollection)
-            {
-                foreach (var ym in ymCollection)
-                {
-                    Console.WriteLine(ym);
-                }
-            }
-
-            static YearMonth First(YearMonth[] yms)
-            {
-                foreach (var ym in yms)
-                {
-                    if (ym.Is21Century)
-                        return ym;
-                }
-                return null;
-            }
-
-            static void Two(YearMonth[] yms)
-            {
-                var yearmonth = First(yms);
-                if (yearmonth == null)
-                {
-                    Console.WriteLine("21世紀は存在しません");
-                }
-                else
-                {
-                    Console.WriteLine(yearmonth);
-                }
-            }
-
-            static void Three(YearMonth[] yms)
-            {
-                var array = yms.Select(n => n.AddOneMonth())
-                               .ToArray();
-                foreach (var ym in array)
-                {
-                    Console.WriteLine(ym);
-                }
-            }
+            
 
         }
 
     }
 
-    public class YearMonth
-    {
-        public int Year { get; private set; }
-
-        public int Month { get; private set; }
-
-        public YearMonth(int year, int month)
-        {
-            Year = year;
-            Month = month;
-        }
-
-        public bool Is21Century
-        {
-            get
-            {
-                return 2001 <= Year && Year <= 2100;
-            }
-        }
-
-        public YearMonth AddOneMonth()
-        {
-            if (Month == 12)
-            {
-                return new YearMonth(this.Year + 1, 1);
-            }
-            else
-            {
-                return new YearMonth(this.Year, this.Month + 1);
-            }
-        }
-
-        public override string ToString()
-        {
-            return $"{Year}年{Month}月";
-        }
-
-    }
 }
  
